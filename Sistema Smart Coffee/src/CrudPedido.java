92fbb5122b186c0272d42443f66ed6232c66099a
MarjorieValdivieso <marjorievillacis430@gmail.com>
2026-05-31 11:24:58 -0500

    clase Interfaz

---
+++ Sistema Smart Coffee/src/CrudPedido.java
@@ -0,0 +1,11 @@
+public interface CrudPedido {
+    void registrarPedido(String correo, double consumo);
+
+    void actualizarPedido(String correo, double nuevoConsumo);
+
+    void eliminarPedido(String correo);
+
+    double promedioConsumo();
+
+    void mejorCliente();
+}
